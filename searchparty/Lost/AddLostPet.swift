6257a7ec695c69375c448193d2e62cfca1e575a8
Cameron Henige <godmodecameron@gmail.com>
2021-05-02 23:08:56 -0400

    images working sort of

--- searchparty/Lost/AddLostPet.swift
+++ searchparty/Lost/AddLostPet.swift
@@ -8,12 +8,20 @@
 
 import SwiftUI
 import Combine
+import PhotosUI
 
 struct AddLostPet: View {
+    @State private var showingActionSheet = false
+       @State private var backgroundColor = Color.white
 
     @State var petName = ""
     @State var petAge = ""
     @State var petBreed = ""
+    @State private var isShowPhotoLibrary = false
+    @State private var isShowCamera = false
+    @State private var image = UIImage()
+    @State private var images : [UIImage] = []
+    @State var picker = false
 
     @State private var petType = 0
     var petTypes = ["Dog", "Cat", "Bird", "Other"]
@@ -51,18 +59,107 @@ struct AddLostPet: View {
                     }
                     
                     TextField("Breed", text: $petBreed)
+                    
+                    if !images.isEmpty{
+                        ForEach(images,id: \.self){ img in
+                            Image(uiImage: img).resizable().frame(width: UIScreen.main.bounds.width-45, height: 250).cornerRadius(20)
+                        }
+                    }
+                    Image(uiImage: self.image)
+                        .resizable()
+                        .frame(minWidth: 0, maxWidth: .infinity, maxHeight: 100.0)
+                    
+                    
+                    Button(action: {
+                        self.showingActionSheet = true
+                        //self.picker = true
+                    }) {
+                        Text("Add Image")
+                    }.buttonStyle(PrimaryButtonStyle()).padding([.top, .leading])
 
                 }
         
                 
+        }.actionSheet(isPresented: $showingActionSheet) {
+            ActionSheet(title: Text("Choose Photo Location"), message: Text("Select photo location"), buttons: [
+                .default(Text("Gallery")) {
+                    self.showingActionSheet = false
+                    self.picker = true },
+                .default(Text("Camera")) {
+                    //todo
+                    self.isShowCamera = true },
+                .cancel()
+            ])
         }.navigationTitle("Add Lost Pet").navigationBarColor(Constant.color.tintColor.uiColor())
+        .sheet(isPresented: $picker) {
+            MyImagePicker(images: $images, picker: $picker)
+        }.sheet(isPresented: $isShowCamera) {
+            ImagePicker(sourceType: .camera, selectedImage: self.$image)
+            
+        }
 
         }
     
 }
 
-struct AddLostPet_Previews: PreviewProvider {
-    static var previews: some View {
-        AddLostPet()
+struct MyImagePicker : UIViewControllerRepresentable {
+    @Binding var images: [UIImage]
+    @Binding var picker: Bool
+    
+    func makeCoordinator() -> Coordinator {
+        return MyImagePicker.Coordinator(parent1: self)
     }
+    
+    
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        config.selectionLimit = 0
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        
+    }
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate{
+        var parent: MyImagePicker
+        
+        init(parent1: MyImagePicker) {
+            parent = parent1
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            parent.picker.toggle()
+            for img in results{
+                
+            if img.itemProvider.canLoadObject(ofClass: UIImage.self){
+                    
+                img.itemProvider.loadObject(ofClass: UIImage.self) { (image, err) in
+                
+                    guard let image1 = image else {
+                        print(err)
+                        return
+                    }
+                    
+                    self.parent.images.append(image as! UIImage)
+                    
+                }
+            } else{
+                print("cannot be loaded")
+                
+            }
+            }
+            
+        }
+    }
+    
 }
+
+//struct AddLostPet_Previews: PreviewProvider {
+//    static var previews: some View {
+//        AddLostPet()
+//    }
+//}
