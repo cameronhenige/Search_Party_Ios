589f7c769101ad33590ddb5697e7af3ddcbabe13
Cameron Henige <godmodecameron@gmail.com>
2021-05-19 13:08:34 -0400

    save

--- searchparty/Lost/SearchPartyView.swift
+++ searchparty/Lost/SearchPartyView.swift
@@ -8,17 +8,28 @@
 
 import SwiftUI
 import MapKit
+import BottomSheet
 
 struct SearchPartyView: View {
     
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    @State private var bottomSheetPosition: BottomSheetPosition = .middle
 
     
     var body: some View {
         NavigationView {
             VStack {
         
-        Map(coordinateRegion: $region)
+        Map(coordinateRegion: $region).bottomSheet(bottomSheetPosition: self.$bottomSheetPosition, content: {
+            //The Numbers from 0 to 99 as Main Content in a Scroll View
+            ScrollView {
+                ForEach(0..<100) { index in
+                    Text(String(index))
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .padding(.top)
+        })
         }
 
         }
